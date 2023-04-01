5a036930e527f4c0692114df8b2739bc9147e9b1
EvertonMicael2007 <evertonmicael2007@gmail.com>
2023-03-31 22:54:50 -0300

    iPhone

---
+++ icripts.js
@@ -0,0 +1,12 @@
+
+function trocacor(cor) {
+    let circulo = document.querySelector(".circulo")
+    circulo.style.background = cor
+    console. log(circulo)
+}
+
+function trocaimg(imagem){
+    let imgiPhone = document.querySelector(".iPhone")
+    imgiPhone.src = imagem
+
+}
